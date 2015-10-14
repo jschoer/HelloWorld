3eeb819f0d92d864ff2cf949b29c306ad9778a84
Joshua Schoerverth <josh57bel@yahoo.com>
2015-10-13 20:28:52 -0400

    Added Stuff
    Added all my stuff for hello world

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,16 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//Joshua Schoerverth
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 0;
+	cout << "Hello World!\n" << "Input a value for x: ";
+	cin >> x;
+	cout << "\nYou inputed x = " << x;
+    return 0;
+}
+
